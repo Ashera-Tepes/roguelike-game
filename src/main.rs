69a814149c7f2ff3c5803acb1eb0ef68928b2bf9
Ashera Tepes <101460011+Ashera-Tepes@users.noreply.github.com>
2022-03-17 17:29:02 +0530

    Created basic random map generator

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
-//
+//----------------------------------------------------
+// Component Section
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -20,6 +22,78 @@ struct Renderable {
 #[derive(Component, Debug)]
 struct Player {}
 
+//----------------------------------------------------
+// Map Section
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y * 80 + x) as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80 * 50];
+
+    // Make the boundary walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // 400 Random walls
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let player_starting_position = xy_idx(40, 25);
+
+    for _i in 0..400 {
+        let (x, y) = (rng.roll_dice(1, 79), rng.roll_dice(1, 49));
+        let idx = xy_idx(x, y);
+        if idx != player_starting_position {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let (mut x, mut y) = (0, 0);
+    let grey = RGB::from_f32(0.5, 0.5, 0.5);
+    let green = RGB::from_f32(0.0, 1.0, 0.0);
+    let black = RGB::from_f32(0., 0., 0.);
+    let floor = rltk::to_cp437('.');
+    let wall = rltk::to_cp437('#');
+
+    for tile in map.iter() {
+        // Rendering a tile depending on tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, grey, black, floor);
+            }
+            TileType::Wall => {
+                ctx.set(x, y, green, black, wall);
+            }
+        }
+
+        // Move the coordinate
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+//----------------------------------------------------
+// World State Section
+
 struct State {
     ecs: World,
 }
@@ -30,6 +104,9 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_system();
 
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -39,13 +116,26 @@ impl GameState for State {
     }
 }
 
+impl State {
+    fn run_system(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
+//----------------------------------------------------
+// Player Section
+
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
+    let players = ecs.read_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+    for (_player, pos) in (&players, &mut positions).join() {
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -63,11 +153,7 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
-impl State {
-    fn run_system(&mut self) {
-        self.ecs.maintain();
-    }
-}
+//----------------------------------------------------
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
@@ -77,6 +163,8 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
