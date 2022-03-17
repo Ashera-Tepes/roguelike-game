7900e1a77e983a2f25ffd80d0af5b0b2ccc8c803
Ashera Tepes <101460011+Ashera-Tepes@users.noreply.github.com>
2022-03-17 15:43:49 +0530

    Added player movement using arrow keys

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
+//
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -16,12 +17,18 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State {
     ecs: World,
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        player_input(self, ctx);
+        self.run_system();
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -32,6 +39,36 @@ impl GameState for State {
     }
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player Movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
+impl State {
+    fn run_system(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike").build()?;
@@ -39,6 +76,7 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -48,6 +86,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
     rltk::main_loop(context, gs)
 }
