7542e75aa5de224fb465a6777b784bdb202c2b88
Ashera Tepes <101460011+Ashera-Tepes@users.noreply.github.com>
2022-03-18 22:57:24 +0530

    Slightly cleaner code

--- src/lib/player.rs
+++ src/lib/player.rs
@@ -1,4 +1,4 @@
-use rltk::VirtualKeyCode;
+use rltk::VirtualKeyCode::*;
 use specs::prelude::*;
 use std::cmp::{max, min};
 
@@ -23,25 +23,13 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
-            VirtualKeyCode::Left
-            | VirtualKeyCode::Numpad4
-            | VirtualKeyCode::A
-            | VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
+            Left | Numpad4 | A | H => try_move_player(-1, 0, &mut gs.ecs),
 
-            VirtualKeyCode::Right
-            | VirtualKeyCode::Numpad6
-            | VirtualKeyCode::D
-            | VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
+            Right | Numpad6 | D | L => try_move_player(1, 0, &mut gs.ecs),
 
-            VirtualKeyCode::Up
-            | VirtualKeyCode::Numpad8
-            | VirtualKeyCode::W
-            | VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
+            Up | Numpad8 | W | K => try_move_player(0, -1, &mut gs.ecs),
 
-            VirtualKeyCode::Down
-            | VirtualKeyCode::Numpad2
-            | VirtualKeyCode::S
-            | VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
+            Down | Numpad2 | S | J => try_move_player(0, 1, &mut gs.ecs),
 
             _ => {}
         },
